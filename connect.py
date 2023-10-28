7d380f1ef099846709d40c98492695b4992cc983
esswxq <farewellnay@gmail.com>
2023-10-28 17:51:53 +1300

    initial settings to db

--- connect.py
+++ connect.py
@@ -1,7 +1,7 @@
 # These details are available on the first MySQL Workbench screen
 # Usually called 'Local Instance'
-dbuser = "naomiwu" # Your MySQL username - likely 'root'
+dbuser = "root" # Your MySQL username - likely 'root'
 dbpass = "W8228095xq" # ---- PUT YOUR PASSWORD HERE ----
-dbhost = "naomiwu.mysql.pythonanywhere-services.com"
+dbhost = "localhost"
 dbport = "3306"
-dbname = "naomiwu$brmm"
+dbname = "motorkhana"
